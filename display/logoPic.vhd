24fe74b38961b1380a7da76ca4e55e57813e58a4
Clarence Wang <chwangthu@gmail.com>
2018-06-10 10:36:22 +0800

    状态界面显示

---
+++ display/logoPic.vhd
@@ -0,0 +1,52 @@
+-- Read from rom and return rgb value to render
+-- logo : 320 * 51
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.numeric_std.all;
+use work.types.all;
+
+entity logoPic is
+	port(
+		pix_x, pix_y : in integer range 0 to 1000;
+		state: in std_logic_vector(2 downto 0);
+		ply_num : in std_logic; --玩家编号，0为A，1为B
+		pixel_out: out Pixel;
+		clk : in std_logic
+	);
+end entity logoPic;
+
+architecture bhv_logoPic of logoPic is
+
+	component logo IS
+	PORT
+		(
+			address		: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+			clock		: IN STD_LOGIC  := '1';
+			q		: OUT STD_LOGIC_VECTOR (9 DOWNTO 0)
+		);
+	END component logo;
+
+signal addrLogo : std_logic_vector(15 downto 0) := (others => '0');
+signal rgbaLogo : std_logic_vector(9 downto 0);
+signal addrLogoInt : integer range 0 to 70000 := 0;
+
+begin
+	u1 : logo port map(address => addrLogo, clock => clk, q => rgbaLogo);
+
+
+	addrLogoInt <= (pix_y - 150) * 280 + (pix_x - 180) when (180 <= pix_x and pix_x < 460 and 150 <= pix_y and pix_y < 190) else
+						(pix_y - 230) * 280 + (pix_x - 180) + 280 * 40 when (180 <= pix_x and pix_x < 460 and 230 <= pix_y and pix_y < 270 and (state = "000" or (state = "001" and ply_num = '0') or (state = "010" and ply_num = '1'))) else
+						(pix_y - 230) * 280 + (pix_x - 180) + 280 * 40 * 2 when (180 <= pix_x and pix_x < 460 and 230 <= pix_y and pix_y < 270 and ((state = "001" and ply_num = '1') or (state = "010" and ply_num = '0'))) else
+						(pix_y - 230) * 280 + (pix_x - 180) + 280 * 40 * 3 when (180 <= pix_x and pix_x < 460 and 230 <= pix_y and pix_y < 270 and state = "101") else
+						(pix_y - 230) * 280 + (pix_x - 180) + 280 * 40 * 4 when (180 <= pix_x and pix_x < 460 and 230 <= pix_y and pix_y < 270 and state = "110") else
+					   70000;
+	addrLogo <= conv_std_logic_vector(addrLogoInt, 16);
+	
+	pixel_out.r(2) <= rgbaLogo(9); pixel_out.r(1) <= rgbaLogo(8); pixel_out.r(0) <= rgbaLogo(7);
+	pixel_out.g(2) <= rgbaLogo(6); pixel_out.g(1) <= rgbaLogo(5); pixel_out.g(0) <= rgbaLogo(4);
+	pixel_out.b(2) <= rgbaLogo(3); pixel_out.b(1) <= rgbaLogo(2); pixel_out.b(0) <= rgbaLogo(1);
+	pixel_out.valid <= false when (rgbaLogo(0) = '0' or addrLogoInt = 70000)
+									 else true;
+
+end architecture;
--- statics/logo.vhd
+++ statics/logo.vhd
@@ -42,7 +42,7 @@ USE altera_mf.all;
 ENTITY logo IS
 	PORT
 	(
-		address		: IN STD_LOGIC_VECTOR (13 DOWNTO 0);
+		address		: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
 		clock		: IN STD_LOGIC  := '1';
 		q		: OUT STD_LOGIC_VECTOR (9 DOWNTO 0)
 	);
@@ -72,7 +72,7 @@ ARCHITECTURE SYN OF logo IS
 		width_byteena_a		: NATURAL
 	);
 	PORT (
-			address_a	: IN STD_LOGIC_VECTOR (13 DOWNTO 0);
+			address_a	: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
 			clock0	: IN STD_LOGIC ;
 			q_a	: OUT STD_LOGIC_VECTOR (9 DOWNTO 0)
 	);
@@ -85,15 +85,15 @@ BEGIN
 	GENERIC MAP (
 		clock_enable_input_a => "BYPASS",
 		clock_enable_output_a => "BYPASS",
-		init_file => "../logo(1).mif",
+		init_file => "./all.mif",
 		intended_device_family => "Cyclone II",
 		lpm_hint => "ENABLE_RUNTIME_MOD=NO",
 		lpm_type => "altsyncram",
-		numwords_a => 16384,
+		numwords_a => 65536,
 		operation_mode => "ROM",
 		outdata_aclr_a => "NONE",
 		outdata_reg_a => "CLOCK0",
-		widthad_a => 14,
+		widthad_a => 16,
 		width_a => 10,
 		width_byteena_a => 1
 	)
@@ -127,35 +127,35 @@ END SYN;
 -- Retrieval info: PRIVATE: JTAG_ENABLED NUMERIC "0"
 -- Retrieval info: PRIVATE: JTAG_ID STRING "NONE"
 -- Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
--- Retrieval info: PRIVATE: MIFfilename STRING "../logo.mif"
--- Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "16384"
+-- Retrieval info: PRIVATE: MIFfilename STRING "../logo(1).mif"
+-- Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "65536"
 -- Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "0"
 -- Retrieval info: PRIVATE: RegAddr NUMERIC "1"
 -- Retrieval info: PRIVATE: RegOutput NUMERIC "1"
 -- Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
 -- Retrieval info: PRIVATE: SingleClock NUMERIC "1"
 -- Retrieval info: PRIVATE: UseDQRAM NUMERIC "0"
--- Retrieval info: PRIVATE: WidthAddr NUMERIC "14"
+-- Retrieval info: PRIVATE: WidthAddr NUMERIC "16"
 -- Retrieval info: PRIVATE: WidthData NUMERIC "10"
 -- Retrieval info: PRIVATE: rden NUMERIC "0"
 -- Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_INPUT_A STRING "BYPASS"
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_A STRING "BYPASS"
--- Retrieval info: CONSTANT: INIT_FILE STRING "../logo.mif"
+-- Retrieval info: CONSTANT: INIT_FILE STRING "../logo(1).mif"
 -- Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone II"
 -- Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=NO"
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
--- Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "16384"
+-- Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "65536"
 -- Retrieval info: CONSTANT: OPERATION_MODE STRING "ROM"
 -- Retrieval info: CONSTANT: OUTDATA_ACLR_A STRING "NONE"
 -- Retrieval info: CONSTANT: OUTDATA_REG_A STRING "CLOCK0"
--- Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "14"
+-- Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "16"
 -- Retrieval info: CONSTANT: WIDTH_A NUMERIC "10"
 -- Retrieval info: CONSTANT: WIDTH_BYTEENA_A NUMERIC "1"
--- Retrieval info: USED_PORT: address 0 0 14 0 INPUT NODEFVAL "address[13..0]"
+-- Retrieval info: USED_PORT: address 0 0 16 0 INPUT NODEFVAL "address[15..0]"
 -- Retrieval info: USED_PORT: clock 0 0 0 0 INPUT VCC "clock"
 -- Retrieval info: USED_PORT: q 0 0 10 0 OUTPUT NODEFVAL "q[9..0]"
--- Retrieval info: CONNECT: @address_a 0 0 14 0 address 0 0 14 0
+-- Retrieval info: CONNECT: @address_a 0 0 16 0 address 0 0 16 0
 -- Retrieval info: CONNECT: @clock0 0 0 0 0 clock 0 0 0 0
 -- Retrieval info: CONNECT: q 0 0 10 0 @q_a 0 0 10 0
 -- Retrieval info: GEN_FILE: TYPE_NORMAL logo.vhd TRUE
